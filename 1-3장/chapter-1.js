316ed7010d539cf9621e7121e27bda85e7ae3ff9
박상민 <psm0102psm@gmail.com>
2021-12-15 22:10:23 +0900

    chore: amountFor 함수 이동

--- 1-3장/chapter-1.js
+++ 1-3장/chapter-1.js
@@ -1,8 +1,50 @@
-function statement(invoice, plays) {
+function statement(data, plays) {
+	const statementData = {}
+	statementData.customer = data.customer
+	statementData.performances = invoice.performances.map(enrichPerformance)
+	return renderPlainText(statementData, plays)
+
+	function enrichPerformance(aPerformance) {
+		const result = Object.assign({}, aPerformance)
+		result.play = playFor(result)
+		result.amount = amountFor(result)
+		return result
+	}
+
+	function playFor(aPerformance) {
+		return plays[aPerformance.playId]
+	}
+
+	function amountFor(aPerformance) {
+		let result = 0
+
+		switch (aPerformance.play.type) {
+			case 'tragedy':
+				result = 40000
+				if (aPerformance.audience > 30) {
+					result += 1000 * (aPerformance.audience - 30)
+				}
+				break
+			case 'comedy':
+				result = 30000
+				if (aPerformance.audience > 20) {
+					result += 10000 + 500 * (aPerformance.audience - 20)
+				}
+				result += 300 * aPerformance.audience
+				break
+			default:
+				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+		}
+
+		return result
+	}
+}
+
+function renderPlainText(data, plays) {
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`
 
-	for (let perf of invoice.performances) {
-		result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)`
+	for (let perf of data.performances) {
+		result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)`
 	}
 
 	result += `총액: ${usd(totalAmount())}\n`
@@ -12,15 +54,15 @@ function statement(invoice, plays) {
 
 	function totalAmount() {
 		let result = 0
-		for (let perf of invoice.performances) {
-			result += amountFor(perf)
+		for (let perf of data.performances) {
+			result += perf.amount
 		}
 		return result
 	}
 
 	function totalVolumeCredits() {
 		let result = 0
-		for (let perf of invoice.performances) {
+		for (let perf of data.performances) {
 			result += volumeCreditsFor(perf)
 		}
 		return result
@@ -38,36 +80,8 @@ function statement(invoice, plays) {
 		let volumeCredits = 0
 
 		volumeCredits += Math.max(perf.audience - 30, 0)
-		if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
+		if (perf.play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
 
 		return volumeCredits
-	}
-
-	function playFor(aPerformance) {
-		return plays[aPerformance.playId]
-	}
-
-	function amountFor(aPerformance) {
-		let result = 0
-
-		switch (playFor(aPerformance).type) {
-			case 'tragedy':
-				result = 40000
-				if (aPerformance.audience > 30) {
-					result += 1000 * (aPerformance.audience - 30)
-				}
-				break
-			case 'comedy':
-				result = 30000
-				if (aPerformance.audience > 20) {
-					result += 10000 + 500 * (aPerformance.audience - 20)
-				}
-				result += 300 * aPerformance.audience
-				break
-			default:
-				throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
-		}
-
-		return result
 	}
 }
